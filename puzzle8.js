ee4b1b0445c521fd51e24914d0296c068984904b
selinalyc <yliu3@cca.edu>
2017-01-22 20:27:40 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,48 @@
+// Help Rudy the Red Dot collect all the green dots!
+
+// Control Rudy with the following commands:
+//  up();                   -- moves Rudy up one square
+//  down();                 -- ...down one square
+//  left();                 -- ...left one square
+//  right();                -- ...right one square
+
+// Interact with Rudy's environment using the following commands:
+//  remainingDots();        -- returns the number of green dots remaining
+//  getColor();             -- returns the color of Rudy's square
+//  setColor(color);        -- changes the color of Rudy's square, opens gates if colors match
+
+var steps=0;
+while(steps<8){
+  down();
+steps=steps+1;
+}
+
+right();
+right();
+var steps=0;
+while(steps<7){
+  up();
+steps=steps+1;
+}
+right();
+right();
+var steps=0;
+while(steps<7){
+  down();
+steps=steps+1;
+}
+right();
+right();
+var steps=0;
+while(steps<7){
+  up();
+steps=steps+1;
+}
+right();
+right();
+var steps=0;
+while(steps<7){
+  down();
+steps=steps+1;
+}
+right();
